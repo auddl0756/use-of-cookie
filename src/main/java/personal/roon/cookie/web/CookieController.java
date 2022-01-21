cd40642d4fbfcaa5e9a7d83277cc0de808d65565
auddl0756 <cjddl789123@naver.com>
2022-01-22 06:04:37 +0900

    @CookieValue 사용해서 방문 횟수 쿠키 주고받기 / @CookieValue를 잘못 사용하고 있었음...

--- src/main/java/personal/roon/cookie/web/CookieController.java
+++ src/main/java/personal/roon/cookie/web/CookieController.java
@@ -1,6 +1,7 @@
 package personal.roon.cookie.web;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,18 +20,27 @@ import java.util.List;
 @RequestMapping("/cookie")
 @Controller
 public class CookieController {
+    private static final int COOKIE_AGE = 60 * 60 * 24;
+
     @GetMapping
     public String cookiePage() {
         return "cookie";
     }
 
     @PostMapping
-    public ModelAndView getCookie(@CookieValue Cookie cookie) throws NullPointerException{
+    public ModelAndView getCookie(@CookieValue(name = "visited_count", defaultValue = "0") int cookieValue, HttpServletResponse servletResponse) {
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("cookie");
 
-        log.info(cookie.getName()+" "+cookie.getValue()+" "+cookie.getMaxAge());
+        Cookie cookie = new Cookie("visited_count", String.valueOf(cookieValue +1));
+        cookie.setPath("/cookie");
+        cookie.setMaxAge(COOKIE_AGE);
 
+        servletResponse.addCookie(cookie);
+        log.info(cookie.getName() + " " + cookie.getValue() + " " + cookie.getMaxAge());
+
+        modelAndView.addObject("cookie", cookie);   //쿠키를 model에 담는거 왜 안 되지?
         return modelAndView;
     }
 }
