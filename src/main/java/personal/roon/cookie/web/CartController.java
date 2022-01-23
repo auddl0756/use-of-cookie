087ec57e2eb212f2346472f0a0322f8f332d48b0
auddl0756 <cjddl789123@naver.com>
2022-01-23 19:04:19 +0900

    쿠키 방식의 장바구니 - 장바구니에서 삭제도 되도록 수정

--- src/main/java/personal/roon/cookie/web/CartController.java
+++ src/main/java/personal/roon/cookie/web/CartController.java
@@ -3,14 +3,13 @@ package personal.roon.cookie.web;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Log4j2
@@ -31,13 +30,24 @@ public class CartController {
         return "cart";
     }
 
-    @ResponseBody
-    @PostMapping
-    public void addProduct(@RequestBody String uuid, @CookieValue(name = COOKIE_NAME, defaultValue = "", required = false) String cartList, HttpServletResponse servletResponse) {
-        String strCartList = cartList+"_"+uuid;
+    private static Set<String> cartSet = new HashSet<>();
 
-        log.info("uuid = " + uuid);
-        log.info(strCartList);
+    @ResponseBody
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addProduct(@RequestBody CartDto cartDto, @CookieValue(name = COOKIE_NAME, defaultValue = "", required = false) String cartList, HttpServletResponse servletResponse) {
+        String uuid = cartDto.getUuid();
+        boolean isAdded = cartDto.isAdded();
+        log.info(cartDto);
+
+        if(isAdded){
+            cartSet.add(uuid);
+        }else{
+            cartSet.remove(uuid);
+        }
+
+        String strCartList = cartSet.stream().collect(Collectors.joining("_"));
+
+        log.info(strCartList+" "+cartSet.size());
 
         Cookie cookie = cookieUtil.makeCookie(COOKIE_NAME, strCartList, COOKIE_VISIBLE_PATH, COOKIE_AGE);
         servletResponse.addCookie(cookie);
